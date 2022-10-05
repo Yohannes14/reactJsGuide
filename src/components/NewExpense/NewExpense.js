efb94d30b475e127d637983c382d2483b0bcdf75
Yohannes14 <yohansdegu.2010@gmail.com>
2022-10-05 14:06:27 -0400

    redering list

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] =useState(false);
 
   const saveExpenseDataHandler =(enteredExpenseData)=>{
     const expenseData ={
       ...enteredExpenseData,
       id: Math.random().toString()
     };
-    //console.log(expenseData);
+    console.log(expenseData);
     props.addExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler=() =>{
+    setIsEditing(true);
+  };
+  const stopEditingHandler=() =>{
+    setIsEditing(false);
   };
   return (
     <div className='new-expense'>
-      <ExpenseForm SaveExpenseData ={saveExpenseDataHandler}/>
+      {!isEditing &&(<button type="submit"
+        onClick={startEditingHandler}>Add New Expense</button>
+)}
+      {isEditing && (<ExpenseForm 
+           saveExpenseData ={saveExpenseDataHandler}
+           onCancel ={stopEditingHandler} />
+)}
     </div>
   );
 };
